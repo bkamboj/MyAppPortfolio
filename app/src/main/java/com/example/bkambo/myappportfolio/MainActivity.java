d9ec2097535e17173c2fd64faee83df13d9df498
bkamboj <bkamboj@sapient.com>
2016-11-02 23:24:11 +0530

    moved dimensions to dimen.xml refactored display toast feature

--- app/src/main/java/com/example/bkambo/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/bkambo/myappportfolio/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.bkambo.myappportfolio;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     private Button mPopularMovies;
     private Button mStockHawk;
     private Button mBuildItBetter;
@@ -18,50 +19,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mPopularMovies = (Button)findViewById(R.id.buttonPopularMovies);
-        mPopularMovies.setOnClickListener(this);
-
-        mStockHawk = (Button)findViewById(R.id.buttonStockHawk);
-        mStockHawk.setOnClickListener(this);
-
-        mBuildItBetter = (Button) findViewById(R.id.buttonBuildItBetter);
-        mBuildItBetter.setOnClickListener(this);
-
-        mMakeAppMaterial = (Button) findViewById(R.id.buttonMakeAppMaterial);
-        mMakeAppMaterial.setOnClickListener(this);
-
-        mGoUbiquitous = (Button) findViewById(R.id.buttonGoUbiquitous);
-        mGoUbiquitous.setOnClickListener(this);
-
-        mCapstone = (Button) findViewById(R.id.buttonCapstone);
-        mCapstone.setOnClickListener(this);
-
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.buttonPopularMovies:
-                Toast.makeText(this,"This button will launch popular movies project",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.buttonStockHawk:
-                Toast.makeText(this,"This button will launch stock hawk project",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.buttonBuildItBetter:
-                Toast.makeText(this,"This button will launch build it better project",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.buttonMakeAppMaterial:
-                Toast.makeText(this,"This button will launch make app material project",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.buttonGoUbiquitous:
-                Toast.makeText(this,"This button will launch go ubiquitous project",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.buttonCapstone:
-                Toast.makeText(this,"This button will launch my capstone project",Toast.LENGTH_SHORT).show();
-                break;
-            default:
-                Toast.makeText(this,"Invalid selection",Toast.LENGTH_SHORT).show();
-                break;
-        }
+    public void displayToast(View view){
+        Button button = (Button) view;
+        String buttonText = (String) button.getText();
+
+        Context context = getApplicationContext();
+
+        CharSequence text = getString(R.string.open_app) + " " + buttonText + " project";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+
     }
 }
